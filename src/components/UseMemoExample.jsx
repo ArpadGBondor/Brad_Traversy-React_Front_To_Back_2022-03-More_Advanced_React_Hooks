2f83f549a5d72970bdfc55a33f7c94934fbc0fdb
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2022-03-20 21:36:34 +0000

    useMemo example

---
+++ src/components/UseMemoExample.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect, useRef, useMemo } from 'react';
+
+const useMemoExample = () => {
+    const [number, setNumber] = useState(1);
+    const [increment, setIncrement] = useState(0);
+    const renders = useRef(1);
+
+    // const sqrt = getSqrt(number);
+    const sqrt = useMemo(() => getSqrt(number), [number]);
+
+    useEffect(() => {
+        renders.current = renders.current + 1;
+    });
+
+    const handleButtonClick = (e) => {
+        setIncrement((prev) => prev + 1);
+    };
+    return (
+        <div>
+            <input
+                className="form-control w-25"
+                type="number"
+                value={number}
+                onChange={(e) => setNumber(e.target.value)}
+            />
+            <h2 className="my-3">
+                The sqrt of {number} is {sqrt}.
+            </h2>
+            <button className="btn btn-primary" onClick={handleButtonClick}>
+                Re Render
+            </button>
+            <h3>Component rendered {renders.current} times.</h3>
+        </div>
+    );
+};
+
+function getSqrt(number) {
+    for (let i = 0; i < 10000; i++) {
+        console.log(i);
+    }
+    console.log('Expensive Function Called!');
+    return Math.sqrt(number);
+}
+
+export default useMemoExample;
